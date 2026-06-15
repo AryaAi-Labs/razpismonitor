536999cbe5c62c3ecfe58821292404df15c9681e
AryaAi-Labs <ploncaric@gmail.com>
2026-06-15 17:16:29 +0200

    Update scraper.py

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -319,7 +319,7 @@ try:
         ted_url,
         json={
             "query": "FT=bolts OR FT=nuts OR FT=fasteners OR FT=screws OR FT=washers AND PD=[20260101 TO 20261231]",
-            "fields": ["publication-number", "OPP-021-Contract", "BT-5131-Part", "deadline-receipt-tender-date-lot", "organisation-country-buyer", "title", "description"],
+            "fields": ["publication-number", "OPP-021-Contract", "BT-5131-Part", "deadline-receipt-tender-date-lot", "organisation-country-buyer", "description-glo"],
             "limit": 50,
             "page": 1,
             "onlyLatestVersions": True
